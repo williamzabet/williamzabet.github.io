45698ed7a5e9c8623e55cc0237a8ee9c9d5f18bc
williamzabet <48535302+williamzabet@users.noreply.github.com>
2020-03-03 18:09:37 -0500

    Create main.js

---
+++ lab_4/scripts/main.js
@@ -0,0 +1,28 @@
+/* 
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+const myTitle = document.querySelector('title');
+myTitle.textContent = 'Hello world!';
+
+var element = document.querySelector('.content');
+
+var p1 = document.createElement('p');
+p1.className = 'para1';
+p1.innerHTML = 'My name has 7 characters';
+
+var p2 = document.createElement('p');
+p2.className = 'para2';
+p2.innerHTML = 'The 3rd character in my name is E';
+
+element.appendChild(p1);
+element.appendChild(p2);
+
+
+*/
+
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+
+
