2fd6aa1a1b113f77c26d729ab23b9d4266f92cb0
williamzabet <48535302+williamzabet@users.noreply.github.com>
2020-03-03 20:59:23 -0500

    Update main.js

--- lab_4/scripts/main.js
+++ lab_4/scripts/main.js
@@ -1,28 +1,34 @@
-/* 
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
 
-const myTitle = document.querySelector('title');
-myTitle.textContent = 'Hello world!';
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
 
-var element = document.querySelector('.content');
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 
-var p1 = document.createElement('p');
-p1.className = 'para1';
-p1.innerHTML = 'My name has 7 characters';
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
 
-var p2 = document.createElement('p');
-p2.className = 'para2';
-p2.innerHTML = 'The 3rd character in my name is E';
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
 
-element.appendChild(p1);
-element.appendChild(p2);
-
-
-*/
-
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
-
-
+myButton.onclick = function() {
+  setUserName();
+}
