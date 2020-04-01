d2ea69ecd13243af53fc0ff121a8f1fddca39ca8
williamzabet <48535302+williamzabet@users.noreply.github.com>
2020-03-31 20:04:59 -0400

    Create main.js

---
+++ A2/main.js
@@ -0,0 +1,35 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+//reference: https://mdn.github.io/learning-area/javascript/building-blocks/gallery/
+
+for(let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(replace) {
+      displayedImage.src = replace.target.src;
+    }
+  }
+
+
+/* Wiring up the Darken/Lighten button */
+
+btn.onclick = function() {
+    const bclass = btn.getAttribute('class');
+    if(bclass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } 
+    else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  }
+  
